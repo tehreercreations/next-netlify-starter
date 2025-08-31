b9c9e126dc08f1bbb7f7229e5e5d4284e2f5fb13
tehreercreations <tehreercreations@gmail.com>
2025-08-31 22:20:35 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '@styles/globals.css'
+import Head from 'next/head'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Tehreer Creations</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* if using PNG instead:
+        <link rel="icon" type="image/png" href="/favicon.png" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default Application
